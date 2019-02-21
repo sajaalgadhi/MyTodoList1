b171e6712341e38f1cefcb1c2eee2d13b0fb77cf
sajaalgadhi <saja.algadhi@outlook.com>
2019-02-21 13:09:55 +0300

    Updates

---
+++ precache-manifest.4fb4ef871b91053bd34836fe6f241397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad41a262f108bf1a51d",
+    "url": "/MyTodoList1/static/css/main.b36c5d66.chunk.css"
+  },
+  {
+    "revision": "7ad41a262f108bf1a51d",
+    "url": "/MyTodoList1/static/js/main.7ad41a26.chunk.js"
+  },
+  {
+    "revision": "15b749ef29e0a230be58",
+    "url": "/MyTodoList1/static/js/runtime~main.15b749ef.js"
+  },
+  {
+    "revision": "eb33f06dc1cfe1ec4c75",
+    "url": "/MyTodoList1/static/js/2.eb33f06d.chunk.js"
+  },
+  {
+    "revision": "cc0dd4b5f4ce1a745d5505c6de0c8f60",
+    "url": "/MyTodoList1/static/media/thumb-1920-651074.cc0dd4b5.jpg"
+  },
+  {
+    "revision": "66f1473dc77b619841adf3076f80fbb8",
+    "url": "/MyTodoList1/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//MyTodoList1/precache-manifest.5e6bd52cbfbca043de5c6508849194d2.js"
+  "/MyTodoList1/precache-manifest.4fb4ef871b91053bd34836fe6f241397.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("//MyTodoList1/index.html", {
+workbox.routing.registerNavigationRoute("/MyTodoList1/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
